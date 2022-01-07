7c12a5082f2121992f4d6c3d296e955d3ace7f4c
isaiahb <isaiahballah@gmail.com>
2022-01-07 12:37:36 -0500

    adding dist files

---
+++ site/dist/precache-manifest.3ccc4a1120175b234afeb36f2c56814b.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a220102b18d58bdde30",
+    "url": "/css/app.ae2fe93b.css"
+  },
+  {
+    "revision": "3a220102b18d58bdde30",
+    "url": "/js/app.be5b17ac.js"
+  },
+  {
+    "revision": "3efab5b5f7125a04f8e4",
+    "url": "/js/chunk-vendors.5b395513.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "38e76a3b4d92ace6aed761ff13db76ef",
+    "url": "/img/background.38e76a3b.svg"
+  },
+  {
+    "revision": "901efe162d40344016c98c42cf754ea0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "209c8b3981abc315fd76da1d6a51180a",
+    "url": "/assets/folder.svg"
+  },
+  {
+    "revision": "c9c008d00e42ff5af56d35f22e0d6191",
+    "url": "/assets/favicon.png"
+  },
+  {
+    "revision": "f13c220e2759967c981b0e925ce6441f",
+    "url": "/assets/contact.svg"
+  },
+  {
+    "revision": "ac9c3791e6abab9fac47c79f9d7406a9",
+    "url": "/assets/github.svg"
+  },
+  {
+    "revision": "33b698a31f3cc41ebe89ad5555511938",
+    "url": "/assets/foldericon.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/assets/logo.png"
+  },
+  {
+    "revision": "ff3b05964c4861df60365b312ef6c267",
+    "url": "/assets/linkedin.svg"
+  },
+  {
+    "revision": "3c40e6f399245bc912d0eb8115c7304e",
+    "url": "/assets/projects/ballsorter.svg"
+  },
+  {
+    "revision": "f510c18129f6b51f9b436c6643901d56",
+    "url": "/assets/projects/achieve.svg"
+  },
+  {
+    "revision": "e5d2fefc652720bb290aaa7b1d8d6b12",
+    "url": "/assets/projects/hackathons.svg"
+  },
+  {
+    "revision": "7dc668342a8d40946e1aee271cd18269",
+    "url": "/assets/projects/webookrooms.svg"
+  },
+  {
+    "revision": "e4ea77e1e4df2ea7968fa3c34ae0f0ac",
+    "url": "/assets/Terminal.svg"
+  },
+  {
+    "revision": "70195e96f47f5ef71448c15501ee835d",
+    "url": "/assets/projects/welift.svg"
+  },
+  {
+    "revision": "2edb2ab14b83cc5f8fe2c05843cf7304",
+    "url": "/assets/projects/freelancing.svg"
+  },
+  {
+    "revision": "c9c008d00e42ff5af56d35f22e0d6191",
+    "url": "/favicon copy.png"
+  },
+  {
+    "revision": "3e14b1954a5433730c41770a8b9b19f7",
+    "url": "/assets/projects/perceive.svg"
+  },
+  {
+    "revision": "e751d15e158c6cb038b9f78054fffb04",
+    "url": "/assets/projects/cheemaproduce.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "38e76a3b4d92ace6aed761ff13db76ef",
+    "url": "/assets/background.svg"
+  }
+];
